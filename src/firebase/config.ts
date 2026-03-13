eb33465010d846663843c44f4097c4fa286102b5
Lorran Feital <lorranfeital@gmail.com>
2026-03-13 11:27:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7149706608-c1eaa",
+  "appId": "1:881022596830:web:e2142c4189a11e2583344f",
+  "apiKey": "AIzaSyAegzW3JdnCI5cYr4TuVoKc0M8m-QnvHy4",
+  "authDomain": "studio-7149706608-c1eaa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "881022596830"
+};
