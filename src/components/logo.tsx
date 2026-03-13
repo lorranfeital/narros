b0c41f11ffc4a35f5b0052f5f842ca31fdd88f22
Lorran Feital <lorranfeital@gmail.com>
2026-03-13 12:32:47 +0000

    Quando usar logo completa, use o "narros-logo.svg" da pasta public.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,35 +4,33 @@ import Image from "next/image";
 
 type LogoProps = {
   className?: string;
-  textClassName?: string;
   iconClassName?: string;
   showText?: boolean;
 };
 
 export function Logo({
   className,
-  textClassName,
   iconClassName,
   showText = true,
 }: LogoProps) {
   return (
-    <Link href="/" className={cn("flex items-center gap-2.5", className)}>
-      <Image
-        src="/narros-favicon.svg"
-        alt="Narros logo"
-        width={28}
-        height={28}
-        className={cn(iconClassName)}
-      />
-      {showText && (
-        <span
-          className={cn(
-            "font-body text-xl font-bold text-foreground",
-            textClassName
-          )}
-        >
-          Narros
-        </span>
+    <Link href="/" className={cn("flex items-center", className)}>
+      {showText ? (
+        <Image
+          src="/narros-logo.svg"
+          alt="Narros logo"
+          width={100}
+          height={28}
+          className={cn(iconClassName)}
+        />
+      ) : (
+        <Image
+          src="/narros-favicon.svg"
+          alt="Narros logo"
+          width={28}
+          height={28}
+          className={cn(iconClassName)}
+        />
       )}
     </Link>
   );
